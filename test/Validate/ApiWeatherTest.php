42874f583b0547c4e76dc715a4556fa9ba9a0e53
mathiilda <mathilda.tb17@gmail.com>
2020-12-02 11:13:19 +0100

    Updated test for api.

--- test/Validate/ApiWeatherTest.php
+++ test/Validate/ApiWeatherTest.php
@@ -27,7 +27,12 @@ class ApiWeatherTest extends TestCase
         $_POST["ip"] = "172.217.21.142";
         $_POST["test"] = true;
         $res = $this->controller->indexAction();
-        $this->assertIsArray($res);
+
+        if (is_array($res)) {
+            $this->assertIsArray($res);
+        } else {
+            $this->assertIsString($res);
+        }
     }
 
     public function testIndexNull()
